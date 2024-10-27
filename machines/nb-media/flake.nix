c0321e14407b6f04f83f64820949e7e4ddc3f6d9
chaapp1987 <roland@douven-online.de>
2024-10-27 17:02:46 +0100

    Temporary naming fix

--- machines/nb-media/flake.nix
+++ machines/nb-media/flake.nix
@@ -10,11 +10,11 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
-    nixosConfigurations.nb-media = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
+        # so the old configuration file stilnixos displaymanager.utologinl takes effect
         ./configuration.nix
         home-manager.nixosModules.home-manager
           {
