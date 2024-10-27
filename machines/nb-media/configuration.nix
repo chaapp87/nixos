8a20a78aabbd4dfbbd3b9a69382418d9975d66bd
chaapp1987 <roland@douven-online.de>
2024-10-27 11:13:52 +0100

    Initial commit

---
+++ machines/nb-media/configuration.nix
@@ -0,0 +1,257 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # List packages installed in system profile. To arch, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    #  wget
+    git
+    vim
+    htop
+    neofetch
+    nnn # terminal file manager
+
+    kdePackages.kate
+    gnome.gnome-keyring
+    cmake
+    gnumake
+    gcc
+    libtool
+
+     # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
+
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils  # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    glow # markdown previewer in terminal
+
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    lsof # list open files
+
+    # system tools
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+
+  ];
+  # Bootloader.
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+  };
+
+    networking = {
+      hostName = "rd-nb-nixos"; # Define your hostname.
+      #wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+      # Enable networking
+      networkmanager.enable = true;
+    };
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "de_DE.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services = {
+
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "de";
+        variant = "";
+      };
+    };
+    # Enable the KDE Plasma Desktop Environment.
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+    # Enable Gnome-Keyring
+    gnome.gnome-keyring.enable = true;
+    # Enable Gnome KEyring PAM in sddm
+    teamviewer.enable = true;
+    printing.enable = true;
+    blueman.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+      
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+  };
+
+    security.pam.services.sddm.enableGnomeKeyring = true;
+
+  # Enable teamviewer service
+
+
+  # Configure console keymap
+  console.keyMap = "de";
+
+  # Enable CUPS to print documents.
+
+
+  # Enable bluetooth
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.chaapp = {
+    isNormalUser = true;
+    linger = true;
+    description = "chaapp";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Steam 32 Bit fix
+  programs.steam.enable = true; 
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  systemd = {
+    user.services.ssh-agent = {
+      enable = true;
+      after = [ "network.target" ];
+      wantedBy = [ "default.target" ];
+      description = "SSH key agent";
+      serviceConfig = {
+        Type = "simple";
+        Environment=''SSH_AUTH_SOCK=%t/ssh-agent.socket'';
+        ExecStart = ''/run/current-system/sw/bin/ssh-agent -D -a $SSH_AUTH_SOCK'';
+      };
+    };
+
+    user.services.syncthing = {
+      enable = true;
+      after = [ "network.target" ];
+      wantedBy = [ "default.target" ];
+      description = "Syncthing";
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = ''/etc/profiles/per-user/chaapp/bin/syncthing'';
+      };
+    };
+  };
+
+
+
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+
+# Environment variables
+
+}
---
+++ machines/rd-nb-nixos/home.nix
@@ -0,0 +1,204 @@
+{ config, pkgs, ... }:
+
+{
+  # TODO please change the username & home directory to your own
+  home.username = "chaapp";
+  home.homeDirectory = "/home/chaapp";
+
+  home.sessionVariables = {
+  SSH_AUTH_SOCK="/run/user/1000/ssh-agent.socket";
+ 
+};
+  
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    
+    keepassxc
+    nextcloud-client
+    nil
+    steam
+    moonlight-qt
+    ansible
+    teamviewer
+    yubioath-flutter
+    syncthing
+    syncthing-tray
+    retroarchFull
+    dmenu
+    discord
+    ferdium
+    thunderbird
+    chromium
+    
+   
+  ];
+
+
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "chaapp1987";
+    userEmail = "roland@douven-online.de";
+  };
+
+
+
+
+# Mailaccount
+
+  # starship - an customizable prompt for any shell
+#  programs.starship = {
+#    enable = true;
+#    # custom settings
+#    settings = {
+ #     add_newline = false;
+ #     aws.disabled = true;
+ #     gcloud.disabled = true;
+ #     line_break.disabled = true;
+ #   };
+ # };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # TODO add your custom bashrc here
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+
+    # set some aliases, feel free to add more or remove some
+    shellAliases = {
+      k = "kubectl";
+      urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+      urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+      ec = "emacsclient -c -n -a ''";
+    };
+  };
+
+  programs.firefox = {
+    enable = true;
+    profiles.chaapp.name = "chaapp";
+    profiles.chaapp.id = 0;
+    profiles.chaapp.isDefault = true;
+    profiles.chaapp.bookmarks = [
+      {
+        name = "wikipedia";
+        tags = [ "wiki" ];
+        keyword = "wiki";
+        url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
+      }
+      {
+        name = "Nix sites";
+        toolbar = true;
+        bookmarks = [
+          {
+            name = "homepage";
+            url = "https://nixos.org/";
+          }
+          {
+            name = "wiki";
+            tags = [ "wiki" "nix" ];
+            url = "https://wiki.nixos.org/";
+          }
+          {
+            name = "Nix package search";
+            url = "https://search.nixos.org/packages";
+          }
+          {
+            name = "Home-Manager config search";
+            url = "https://home-manager-options.extranix.com/?query=&release=release-24.05";
+          }
+        ];
+      }
+    ];
+    profiles.chaapp.settings = {
+      "browser.toolbars.bookmarks.visibility" = true;
+      "browser.search.region" = "DE";
+    };
+    
+  };
+    
+  programs.emacs = {
+    enable = true;
+     package = pkgs.emacs29-pgtk;
+  };
+  services.emacs = {
+    enable = true;
+    package = pkgs.emacs29-pgtk;
+  };
+  
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+
+  # Let home Manager install and manage itself.
+
+
+
+  programs.home-manager.enable = true;
+
+  # Link my own dotfiles
+  # emacs
+
+  home.file."${config.xdg.configHome}/emacs/init.el" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfilesnew/roles/emacs/files/init-rd-nb-nixos.el";
+  };
+
+
+# "/home/chaapp/dotfilesnew/roles/emacs/files/init-rd-nb-nixos.el"
+#  home.file.".config/emacs/init.el" = {
+#    source = ../../../roles/emacs/files/init-rd-nb-nixos.el;
+#  };
+    
+
+  
+}
+
+
+
