bc8b0ebb299550bd452a4999284fc28e84826f55
chaapp1987 <roland@douven-online.de>
2024-10-29 20:01:44 +0100

    Added Steamdeck

---
+++ machines/steamdeck-nixos/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-23.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.steamdeck-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file stilnixos displaymanager.utologinl takes effect
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.media = import ./home.nix;
+            home-manager.backupFileExtension = "hm-backup";
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+      ];
+    };
+
+
+      
+  };
+}
